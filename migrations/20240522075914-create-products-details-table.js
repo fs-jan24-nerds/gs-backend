c5dc85848a523ca25a8960e9d6f0e81550788952
Nikita Ganzha <nk8bnj@gmail.com>
2024-05-24 08:54:00 +0300

    fix migration

--- migrations/20240522075914-create-products-details-table.js
+++ migrations/20240522075914-create-products-details-table.js
@@ -10,6 +10,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING,
       },
+      category: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
       namespaceId: {
         allowNull: false,
         type: Sequelize.STRING,
